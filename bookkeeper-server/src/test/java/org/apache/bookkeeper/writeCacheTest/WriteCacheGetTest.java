536e3014b50776ed5a768dee31fbe0d259fdf062
Andrea041 <116633306+Andrea041@users.noreply.github.com>
2024-06-02 12:14:20 +0200

    Implemented tests for WriteCache class on methods: - put - get

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/writeCacheTest/WriteCacheGetTest.java
@@ -0,0 +1,84 @@
+package org.apache.bookkeeper.writeCacheTest;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import org.apache.bookkeeper.bookie.storage.ldb.WriteCache;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class WriteCacheGetTest {
+    private final long ledgerId;
+    private final long entryId;
+    private Class<? extends Exception> exceptionOutput;
+    private ByteBuf outputBuffer;
+    private WriteCache writeCache;
+    private final boolean existingIds;
+
+    public WriteCacheGetTest(long ledgerId, long entryId, Object output, boolean existingIds) {
+        if (output instanceof Class && Exception.class.isAssignableFrom((Class<?>) output)) {
+            this.exceptionOutput = (Class<? extends Exception>) output;
+            this.outputBuffer = Unpooled.EMPTY_BUFFER;
+        } else if (output instanceof ByteBuf) {
+            this.outputBuffer = (ByteBuf) output;
+        } else if (output == null) {
+            this.outputBuffer = Unpooled.EMPTY_BUFFER;
+        }
+
+        this.ledgerId = ledgerId;
+        this.entryId = entryId;
+        this.existingIds = existingIds;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        int capacity = 1024;
+        ByteBuf entry = Unpooled.buffer(capacity);
+        entry.writeBytes(new byte[capacity]);
+
+        return Arrays.asList(new Object[][] {
+                {1, -1, IllegalArgumentException.class, true},
+                {0, 1, null, false},
+                {-1, 0, IllegalArgumentException.class, true},
+                {1, 1, entry, true}
+        });
+    }
+
+    @Before
+    public void setUp() {
+        long maxCacheSize = 1024;
+        writeCache = new WriteCache(UnpooledByteBufAllocator.DEFAULT, maxCacheSize);
+    }
+
+    @Test
+    public void test() {
+        ByteBuf entryRes;
+
+        try {
+            if (existingIds)
+                writeCache.put(ledgerId, entryId, outputBuffer);
+
+            entryRes = writeCache.get(ledgerId, entryId);
+
+            if (existingIds)
+                Assert.assertEquals(outputBuffer, entryRes);
+            else
+                Assert.assertNull(entryRes);
+        } catch (IllegalArgumentException e) {
+            Assert.assertEquals(exceptionOutput, e.getClass());
+        }
+    }
+
+    @After
+    public void tearDown() {
+        writeCache.close();
+    }
+}
---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/writeCacheTest/WriteCachePutTest.java
@@ -0,0 +1,85 @@
+package org.apache.bookkeeper.writeCacheTest;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import org.apache.bookkeeper.bookie.storage.ldb.WriteCache;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class WriteCachePutTest {
+    private final ByteBuf entry;
+    private final long entryId;
+    private final long ledgerId;
+    private WriteCache writeCache;
+    private final long maxCacheSize;
+    private boolean booleanOutput;
+    private Class<? extends Exception> exceptionOutput;
+    private final boolean fullCache;
+
+    public WriteCachePutTest(Object output, Integer capacity, long ledgerId, long entryId, long maxCacheSize, boolean defaultEntry, boolean fullCache) {
+        if (output instanceof Class && Exception.class.isAssignableFrom((Class<?>) output)) {
+            this.exceptionOutput = (Class<? extends Exception>) output;
+        } else if (output instanceof Boolean) {
+            this.booleanOutput = (Boolean) output;
+        }
+
+        this.fullCache = fullCache;
+        this.entryId = entryId;
+        this.ledgerId = ledgerId;
+        this.maxCacheSize = maxCacheSize;
+
+        if (capacity != null)
+            this.entry = Unpooled.buffer(capacity);
+        else if (defaultEntry) {
+            this.entry = Unpooled.EMPTY_BUFFER;
+        } else
+            this.entry = null;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {IllegalArgumentException.class, 1024, 1, -1, 2048, false, false},
+                {IllegalArgumentException.class, 1024, -1, 1, 2048, false, false},
+                {true, 0, 1, 1, 2048, true, false},
+                {NullPointerException.class, null, 1, 1, 2048, false, false},
+                {false, 2048, 1, 1, 1024, false, true},
+                {true, 1024, 1, 1, 2048, false, false}
+        });
+    }
+
+    @Before
+    public void setUp() {
+        writeCache = new WriteCache(UnpooledByteBufAllocator.DEFAULT, maxCacheSize);
+    }
+
+    @Test
+    public void test() {
+        boolean check;
+
+        if (fullCache) {
+            entry.writeBytes(new byte[(int) (maxCacheSize + 1)]);
+        }
+
+        try {
+            check = writeCache.put(ledgerId, entryId, entry);
+            Assert.assertEquals(booleanOutput, check);
+        } catch (Exception e) {
+            Assert.assertEquals(exceptionOutput, e.getClass());
+        }
+    }
+
+    @After
+    public void tearDown() {
+        writeCache.close();
+    }
+}
