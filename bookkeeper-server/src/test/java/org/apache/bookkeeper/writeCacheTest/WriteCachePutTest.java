5e86cc6d80c72b7b859a799496cf7832afef61f0
Andrea041 <116633306+Andrea041@users.noreply.github.com>
2024-06-02 14:57:42 +0200

    Refining and adding comments

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/writeCacheTest/WriteCachePutTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/writeCacheTest/WriteCachePutTest.java
@@ -24,8 +24,9 @@ public class WriteCachePutTest {
     private boolean booleanOutput;
     private Class<? extends Exception> exceptionOutput;
     private final boolean fullCache;
+    private final boolean existingIds;
 
-    public WriteCachePutTest(Object output, Integer capacity, long ledgerId, long entryId, long maxCacheSize, boolean defaultEntry, boolean fullCache) {
+    public WriteCachePutTest(Object output, Integer capacity, long ledgerId, long entryId, long maxCacheSize, boolean defaultEntry, boolean fullCache, boolean existingIds) {
         if (output instanceof Class && Exception.class.isAssignableFrom((Class<?>) output)) {
             this.exceptionOutput = (Class<? extends Exception>) output;
         } else if (output instanceof Boolean) {
@@ -36,10 +37,14 @@ public class WriteCachePutTest {
         this.entryId = entryId;
         this.ledgerId = ledgerId;
         this.maxCacheSize = maxCacheSize;
+        this.existingIds = existingIds;
 
-        if (capacity != null)
+        if (capacity != null) {
             this.entry = Unpooled.buffer(capacity);
+            entry.writeBytes(new byte[capacity]);
+        }
         else if (defaultEntry) {
+            /* Consider this configuration of entry buffer as non-valid because is empty */
             this.entry = Unpooled.EMPTY_BUFFER;
         } else
             this.entry = null;
@@ -48,12 +53,13 @@ public class WriteCachePutTest {
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
-                {IllegalArgumentException.class, 1024, 1, -1, 2048, false, false},
-                {IllegalArgumentException.class, 1024, -1, 1, 2048, false, false},
-                {true, 0, 1, 1, 2048, true, false},
-                {NullPointerException.class, null, 1, 1, 2048, false, false},
-                {false, 2048, 1, 1, 1024, false, true},
-                {true, 1024, 1, 1, 2048, false, false}
+                {IllegalArgumentException.class, 1024, 1, -1, 2048, false, false, true},
+                {IllegalArgumentException.class, 1024, -1, 1, 2048, false, false, true},
+                {true, 0, 1, 1, 2048, true, false, true},
+                {NullPointerException.class, null, 1, 1, 2048, false, false, true},
+                {false, 2048, 1, 1, 1024, false, true, true},
+                {true, 1024, 1, 1, 2048, false, false, true},
+                {true, 1024, 1, 1, 2048, false, false, false}
         });
     }
 
@@ -66,16 +72,29 @@ public class WriteCachePutTest {
     public void test() {
         boolean check;
         ByteBuf tempBuf;
+        ByteBuf tempBufOverwrite;
 
-        if (fullCache)
+        if (fullCache) {
+            entry.clear();
             entry.writeBytes(new byte[(int) (maxCacheSize + 1)]);
+        }
 
         try {
             check = writeCache.put(ledgerId, entryId, entry);
             Assert.assertEquals(booleanOutput, check);
 
             tempBuf = writeCache.get(ledgerId, entryId);
-            if (!fullCache) {
+
+            if (!fullCache && existingIds) {
+                /* Overwrite data */
+                entry.clear();
+                entry.writeBytes(new byte[(int) (128)]);
+
+                writeCache.put(ledgerId, entryId, entry);
+                tempBufOverwrite = writeCache.get(ledgerId, entryId);
+
+                Assert.assertNotEquals(tempBuf.readableBytes(), tempBufOverwrite.readableBytes());
+            } else if (!fullCache) {
                 /* This check is only for put test when cache is not full
                 * because it will not create any new buckets */
                 /* Check if the content is effectively written */
